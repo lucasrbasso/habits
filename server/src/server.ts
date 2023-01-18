fdc01978af419ffc39e2e19bc591aefecb351014
lucasrbasso <lucas.rbasso@outlook.com>
2023-01-18 20:07:38 -0300

    feat: backend routes and react first habit component

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,15 @@
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
 import "./styles/global.css";
 
-function App() {
-  return <div className="bg-zinc-900">oi</div>;
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex font-bold flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
 }
-
-export default App;
